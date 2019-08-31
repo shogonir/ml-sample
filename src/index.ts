254ad19865e5766824ee9dda12c2bc10fd95bf48
Shogo N <shogonir@gmail.com>
2019-09-01 02:12:31 +0900

    minimum project

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log('running...')
+console.log('complete!')
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
